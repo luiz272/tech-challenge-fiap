a71c3faf6115747c1e06f362de4f5cab56a7bc31
JonatasAlves9 <alvesjonatas99@gmail.com>
2024-01-04 10:52:41 -0300

    chore: create example of entity of user

--- TechChallenger/src/Domain/Entities/User.cs
+++ TechChallenger/src/Domain/Entities/User.cs
@@ -1,7 +1,23 @@
 using Domain.Base;
+using Domain.ValueObjects;
 
 namespace Domain.Entities;
 
 public class User : IAggregateRoot
 {
+    public string Name { get; private set; }
+    public CPF CPF { get; private set; }
+
+    public User(string name, CPF cPF)
+    {
+        Name = name;
+        CPF = cPF;
+
+        ValidateEntity();
+    }
+
+    private void ValidateEntity()
+    {
+        AssertionConcern.AssertArgumentNotEmpty(Name, "The name cant be empty");
+    }
 }
