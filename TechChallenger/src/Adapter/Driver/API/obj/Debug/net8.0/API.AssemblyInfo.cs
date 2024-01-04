a784793521691df378e7ac2676773e343655b4f6
JonatasAlves9 <alvesjonatas99@gmail.com>
2024-01-04 17:07:40 -0300

    chore: create folders of hexagonal architecture

--- TechChallenger/src/Adapter/Driver/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ TechChallenger/src/Adapter/Driver/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08146f0be0e3a9b2346bcb5f031d706b9d9f3923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
