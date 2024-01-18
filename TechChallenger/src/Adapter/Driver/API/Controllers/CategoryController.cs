72b59c23dfe070f9630ec3f0e33100cf473b6a5f
luiz272 <luizf.lima@pm.me>
2024-01-17 21:31:45 -0300

    Adicionando controller, repository e use cases para Category

---
+++ TechChallenger/src/Adapter/Driver/API/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+using Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ILogger<CategoriesController> _logger;
+        private readonly ICategoryUseCase _categoryUseCase;
+
+        public CategoriesController(ILogger<CategoriesController> logger, ICategoryUseCase CategoryUseCase)
+        {
+            _logger = logger;
+            _categoryUseCase = CategoryUseCase;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+         
+            return Ok(_categoryUseCase.GetAllCategories());
+        }
+    }
+}
---
+++ TechChallenger/src/Core/Application/UseCases/CategoryUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+
+namespace Application.UseCases;
+
+public class CategoryUseCase : ICategoryUseCase
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryUseCase(ICategoryRepository CategoryRepository)
+    {
+        _categoryRepository = CategoryRepository;
+    }
+
+    public IEnumerable<Category> GetAllCategories()
+    {
+        return _categoryRepository.GetAll();
+    }
+
+}
